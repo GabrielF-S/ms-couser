a404552aac6775f6462d0ed3c615b18474015be2
GabrielF-S <gabrielboni2008@hotmail.com>
2024-10-04 12:30:01 -0300

    Configurado a api Gateway Zuul

--- hr-api-gateway-zuul/src/main/java/br/com/gabriel/hr_api_gateway_zuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/br/com/gabriel/hr_api_gateway_zuul/HrApiGatewayZuulApplication.java
@@ -2,7 +2,12 @@ package br.com.gabriel.hr_api_gateway_zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+
+@EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class HrApiGatewayZuulApplication {
 
